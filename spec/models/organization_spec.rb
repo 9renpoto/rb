14e3158eaf7cf6855770b91e194bd4870553ec00
Keisuke Kan <9renpoto@gmail.com>
2019-04-14 13:18:35 +0900

    Added factory tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,5 +3,8 @@
 require "rails_helper"
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    let(:org) { create(:organization) }
+    it { expect(org).to be_valid }
+  end
 end
